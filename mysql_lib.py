f6e3081128c282ea613525e03df9d0bf4e8b9b9c
fedepacher <fede_pacher@hotmail.com>
2023-04-17 16:08:32 -0300

    add: add documentation to MySQL_Class.py file

--- mysql_lib.py
+++ mysql_lib.py
@@ -3,16 +3,35 @@ from mysql.connector import Error
 
 
 class MySQL_Class:
+    """Class to encapsulate MySQL method"""
 
     def __init__(self, host_name='localhost', user_name='root', password=''):
+        """Constructor of the class.
+
+        Args:
+            host_name (str): Database hostname.
+            user_name (str): Database username.
+            user_password (str): Database password.
+        """
         self._host_name = host_name
         self._user_name= user_name
         self._password = password
+        self._connection = None
+        self._cursor = None
         self.create_server_connection(self._host_name, self._user_name, self._password)
 
 
     def create_server_connection(self, host_name, user_name, user_password):
-        self._connection = None
+        """Create a server connection.
+
+        Args:
+            host_name (str): Database hostname.
+            user_name (str): Database username.
+            user_password (str): Database password.
+
+        Returns:
+            mysql.connector.connection.MySQLConnection: Connection object.
+        """
         try:
             self._connection = mysql.connector.connect(
                 host=host_name,
@@ -27,7 +46,14 @@ class MySQL_Class:
         return self._connection
 
     def create_db_connection(self, db_name):
-        self._connection = None
+        """Create a database connection.
+
+        Args:
+            db_name (str): Database name.
+
+        Returns:
+            mysql.connector.connection.MySQLConnection: Connection object.
+        """
         try:
             self._connection = mysql.connector.connect(
                 host=self._host_name,
@@ -43,6 +69,11 @@ class MySQL_Class:
         return self._connection
 
     def create_database(self, query):
+        """Create a database.
+
+        Args:
+            query (str): Query to create database.
+        """
         self._cursor = self._connection.cursor()
         try:
             self._cursor.execute(query)
@@ -52,6 +83,11 @@ class MySQL_Class:
 
 
     def execute_query(self, query):
+        """Execute the query to the database.
+
+        Args:
+            query (str): Query to create database.
+        """
         self._cursor = self._connection.cursor()
         try:
             self._cursor.execute(query)
@@ -62,6 +98,22 @@ class MySQL_Class:
 
 
     def execute_list_query(self, sql, val):
+        """Execute a list of query to the database. It is used to insert values to the database
+
+        Example:
+            sql = '''
+                INSERT INTO teacher (teacher_id, first_name, last_name, language_1, language_2,
+                dob, tax_id, phone_no) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                '''
+            val = [
+                (7, 'Hank', 'Dodson', 'ENG', None, '1991-12-23', 11111, '+491772345678'),
+                (8, 'Sue', 'Perkins', 'MAN', 'ENG', '1976-02-02', 22222, '+491443456432')
+            ]
+
+        Args:
+            sql (str): Query to excecute in the database.
+            val (list of list): List of value to be executed in the query.
+        """
         self._cursor = self._connection.cursor()
         try:
             self._cursor.executemany(sql, val)
@@ -72,6 +124,11 @@ class MySQL_Class:
 
 
     def read_query(self, query):
+        """Read value of the database.
+
+        Args:
+            query (str): Query to read database.
+        """
         self._cursor = self._connection.cursor()
         result = None
         try:
